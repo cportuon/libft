d533facd99012aa5c2e5f254e836edf4694a6cba
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:53:20 +0200

    Función recien creada

---
+++ ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+int  strncmp(const char *s1, const char *s2, size_t n)
+{
+
+}
+int main(void)
+{
+     const char *str1;
+     const char *str2;
+     size_t n;
+
+     str1 = "";
+     str2 = "";
+     n = ;
+     
+}
