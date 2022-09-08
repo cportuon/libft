aabababd0b83a1d8732ba783f6c2b3054ece0440
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 12:54:36 +0200

    Normi OK

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,20 +6,20 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 11:10:47 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/02 12:57:31 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 11:45:40 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isprint (int c)
+int	ft_isprint(int c)
 {
-	if(c >= 32 && c <= 126)
+	if (c >= 32 && c <= 126)
 		return (1);
 	return (0);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int	c;
 	int d;
@@ -30,4 +30,4 @@ int	main(void)
 		printf("%c es un carácter imprimible", c);
 	else
 		printf("%c no es un carácter imprimible", c);
-}
+}*/
