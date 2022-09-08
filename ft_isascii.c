f2c9268c7e43a18deec019cc6d142ee43f585b41
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:28:19 +0200

    Función recien creada

---
+++ ft_isascii.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 10:21:26 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/02 12:57:34 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isascii (int	c)
+{
+	if(c >= 0 && c < 128)
+		return (0);
+	return(1);
+}
+
+int	main(void)
+{
+	int	c;
+	int d;
+
+	c = 'p';
+	d = ft_isascii(c);
+	if(d == 0)
+		printf("%c es un caracter ASCII", c);
+	else
+		printf("%c no es un caracter ASCII", c);
+}
