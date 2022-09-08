90c8934fe7b09322245c827f8068b027b29652b2
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:35:03 +0200

    Función recien creada

---
+++ ft_toupper.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 13:13:49 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/02 12:57:26 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_toupper(int c)
+{
+	if(c >= 97 && c <= 122)
+	{
+		return(c - 32);
+	}
+	return(c);
+}
+
+int main(void)
+{
+	char unsigned c;
+	char unsigned d;
+
+	c = 'a';
+	d = ft_toupper(c);
+	printf("Cambiamos la letra %c a mayúscula %c", c, d);
+	return (0);
+}
