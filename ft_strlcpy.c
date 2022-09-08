f4086cd26e520228148b03119da4d0c018d5b646
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:33:18 +0200

    Función recien creada

---
+++ ft_strlcpy.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 13:49:36 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/02 15:50:07 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+size_t	ft_strlcpy(char * dst, const char * src, size_t dstsize)
+{
+	size_t	i;
+	int length;
+
+	i = 0;
+	length = ft_strlen(src);
+	if (dstsize > 0)
+	{
+		while(i < (dstsize - 1) && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';            
+	}
+	return(length);
+}
+
+int	main(void)
+{			
+		const char src[] = "Cosme";			
+		char dst[] = "";
+		size_t dstsize;
+
+		dstsize = 2;
+		printf("My fuction\nNúmero de carácteres de %s: %lu\n", src, ft_strlcpy(dst, src, dstsize));
+		printf("Destiny = string 2: %s\n", dst);
+		//printf("Fuction origin\nNúmero de carácteres del string 1: %lu\n", strlcpy(dst, src, dstsize));	
+		//printf("Destiny = string 2: %s\n", dst);
+		return (0);
+}
