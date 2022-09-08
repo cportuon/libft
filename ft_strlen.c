109416f205ef70a59fab39557afe2418824c17b5
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 12:58:04 +0200

    Normi OK

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 11:18:06 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/02 15:01:03 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 12:07:07 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const  char *str)
+size_t	ft_strlen(const	char *str)
 {
-	int i;
+	int	i;
 
-	i= 0;
-	while(str[i] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
