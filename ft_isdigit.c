b64a36b4a59f1df071cc2cc4ecda46d5bc938541
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 12:54:07 +0200

    Normi OK

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,20 +6,20 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:00:32 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/02 12:57:33 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 11:31:20 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 		return (1);
 	return (0);
 }
 
-int main(void)
+/*int main(void)
 {
 	int c;
 	int d;
@@ -30,4 +30,4 @@ int main(void)
 		printf("%c es un número", c);
 	else
 		printf("%c no es un número", c);
-}
+}*/
