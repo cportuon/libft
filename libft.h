fae63bc26d2fab4a0f2eeb3a62b9f5c33f3b5bd0
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 13:26:21 +0200

    Añadida función ft_strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 12:47:29 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/08 12:29:08 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 13:24:29 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+char	*ft_strchr(const char *s, int coin);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char	*str);
+int		strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int coin);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const	char	*str);
 
 #endif
