81e2e1ce9780a5638e282c22624718a2472db792
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:35:35 +0200

    Librería creada

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 12:47:29 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/07 11:29:18 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include<stddef.h>
+# include<stdio.h>
+# include<string.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const  char *str);
+
+#endif
