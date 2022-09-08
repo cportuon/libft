44b2bee30a7ba63b4939ea94471b7f51634c1e02
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:27:31 +0200

    Función recien creada

---
+++ ft_isalpha.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 15:27:12 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/02 12:57:35 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_isalpha(int c)
+{
+	if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return(1);
+	return (0);
+}
+
+int main (void)
+{
+	int c;
+	int d;
+
+	c = 'f';
+	d = ft_isalpha(c);
+
+	if (d == 1)
+		printf("%c es un caracter", c);
+	else
+		printf("%c no es un caracter", c);
+}
