0077d6aaf7642d193de205a63ddb3a28a9414b54
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 12:53:19 +0200

    Normi OK

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,20 +6,20 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:21:26 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/02 12:57:34 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 11:29:45 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isascii (int	c)
+int	ft_isascii(int c)
 {
-	if(c >= 0 && c < 128)
+	if (c >= 0 && c < 128)
 		return (0);
-	return(1);
+	return (1);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int	c;
 	int d;
@@ -30,4 +30,4 @@ int	main(void)
 		printf("%c es un caracter ASCII", c);
 	else
 		printf("%c no es un caracter ASCII", c);
-}
+}*/
