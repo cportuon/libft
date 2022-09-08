9838c471bdd1e4b50b9f8388f702009b92467396
Cosme Portuondo Barcena <cportuon@c3r4s4.42urduliz.com>
2022-09-08 12:59:18 +0200

    Normi Ok

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,7 +6,7 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 13:27:56 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/02 12:57:28 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/08 12:38:43 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ft_tolower(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (c + 32);
-	return(c);
+	return (c);
 }
 
-int main(void)
+/*int main(void)
 {
 	char unsigned c;
 	char  unsigned d;
@@ -27,4 +27,4 @@ int main(void)
 	c = 'A';
 	d = ft_tolower(c);
 	printf("Cambiamos la letra %c en maúsculas %c", c, d);
-}
+}*/
