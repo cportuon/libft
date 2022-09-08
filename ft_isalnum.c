5ee7103fffd5ec3e1eed04c78c2d2b86b828055c
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:26:13 +0200

    Función recien creada

---
+++ ft_isalnum.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 09:43:34 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/07 18:39:46 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalnum(int c)
+{
+	if (c >= 48 && c <= 57 
+			|| c >= 65 && c >= 90 
+			|| c <= 97 && c >= 122)
+		return (1);
+	return(0);
+}
+
+/*int	main(void)
+{
+	int	c;
+	int d;
+
+	c = 'g';
+	d = ft_isalnum(c);
+
+	if (d == 1)
+		printf("%c es alfanúmerico", c);
+	else
+		printf("%c no es alfanúmerico", c);
+}*/
