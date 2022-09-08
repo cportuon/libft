69c4a47697cfc8d3428dc257662413260ede0e38
Cosme Portuondo Barcena <cportuon@c3r2s3.42urduliz.com>
2022-09-08 10:32:06 +0200

    Función recien creada

---
+++ ft_isprint.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 11:10:47 by cportuon          #+#    #+#             */
+/*   Updated: 2022/09/02 12:57:31 by cportuon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isprint (int c)
+{
+	if(c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	int	c;
+	int d;
+
+	c = '6';
+	d = ft_isprint(c);
+	if(d == 1)
+		printf("%c es un carácter imprimible", c);
+	else
+		printf("%c no es un carácter imprimible", c);
+}
