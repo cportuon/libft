3ac8d68848f670e103deb71a4b577d626b2900a5
cosisi <cosisi@MacBook-Pro-de-cosisi.local>
2022-09-09 17:47:10 +0200

    Sin Normi ni Paco

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,20 +14,39 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i
+	int	i;
+     unsigned char *str1;
+     unsigned char *str2;
+
+     i = 0;
+     str1 = (unsigned char *)s1;
+     str2 = (unsigned char *)s2;
+     while ((str1[i] != '\0' || str2[i] != '\0') && (i < n))
+     {
+          if (str1[i] < str2[i])
+          {
+               return (str1[i] - str2[i]);
+          }
+          else if (str1[i] > str2[i])
+          {
+               return (str1[i] - str2[i]);
+          }
+          i++;
+     }
+     return (0);
 }
-int	main(void)
+/*int	main(void)
 {
      const char *str1;
      const char *str2;
      size_t n;
-	 int rst;
+	int rst;
 
      str1 = "cosmeportuondo@gmail.com";
      str2 = "cosmeportubar@gmail.com";
      n = 11;
-	 rst = ft_strncmp(str1, str2, n);
-	 printf("ft_strncmp fuction: %d\n",rst);
-	 rst = strncmp(str1, str2, n);
-	 printf("strncmp fuction: %d\n",rst);
-}
+	rst = ft_strncmp(str1, str2, n);
+	printf("ft_strncmp fuction: %d\n",rst);
+	rst = strncmp(str1, str2, n);
+	printf("strncmp fuction: %d\n",rst);
+}*/
