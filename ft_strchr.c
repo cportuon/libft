b7358abc348495c6845df349fc81602613878434
Cosme Portuondo Barcena <cportuon@c3r6s5.42urduliz.com>
2022-09-09 13:21:56 +0200

    Paco OK

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: cportuon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 11:46:59 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/08 11:51:52 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:07:06 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ char	*ft_strchr(const char *s, int coin)
 			return (&str[i]);
 		i++;
 	}
+	if (coin == '\0')
+		return (&str[i]);
 	return (0);
 }
 /*int main(void)
@@ -34,7 +36,7 @@ char	*ft_strchr(const char *s, int coin)
     char *rst;
 
     str = "cportuon";
-    coin = '0';
+    coin = '\0';
     rst = ft_strchr(str, coin);
     printf("ft_strchr fuction: %s\n", rst);
     rst = strchr(str, coin);
