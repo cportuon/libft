0f2bdda16987f3009f952663faddbf9d2eba9782
Cosme Portuondo Barcena <cportuon@c3r6s5.42urduliz.com>
2022-09-09 13:46:45 +0200

    Fri 09 13:46

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,28 @@
 /*   By: cportuon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:31:38 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/08 13:25:31 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:45:15 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int  ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-
+	int	i
 }
-int main(void)
+int	main(void)
 {
      const char *str1;
      const char *str2;
      size_t n;
+	 int rst;
 
-     str1 = "";
-     str2 = "";
-     n = ;
-     
+     str1 = "cosmeportuondo@gmail.com";
+     str2 = "cosmeportubar@gmail.com";
+     n = 11;
+	 rst = ft_strncmp(str1, str2, n);
+	 printf("ft_strncmp fuction: %d\n",rst);
+	 rst = strncmp(str1, str2, n);
+	 printf("strncmp fuction: %d\n",rst);
 }
