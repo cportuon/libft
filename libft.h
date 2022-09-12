b07a4e9561b87f2f18d4d1a00ef1dba64361c836
Cosme Portuondo Barcena <cportuon@c3r2s6.42urduliz.com>
2022-09-12 19:42:01 +0200

    Funciones añadidas

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 12:47:29 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/08 13:25:45 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/12 17:53:55 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,14 @@ char	*ft_strchr(const char *s, int coin);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char	*str);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strrchr(const char *s, int coin);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
