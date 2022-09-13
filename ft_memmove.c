21d11f5bdf5c5635d68884b3ed39559601a973fa
cosisi <cosisi@MacBook-Pro-de-cosisi.local>
2022-09-13 09:02:02 +0200

    Sin Normi ni Paco

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,28 +2,33 @@
 
 void * ft_memmove(void *dst, const void *src, size_t len)
 {
-    unsigned const char    *origin;
-    unsigned char    *dest;
-    size_t  i;
+    unsigned char       *dest;
+    unsigned const char *origin;
+    size_t              i;
 
-    origin = (unsigned const char *)src;
     dest = (unsigned char *)dst;
+    origin = (unsigned const char *)src;
     i = 0;
-    if (origin == '\0' && dest == '\0')
+    if (!dest && !origin)
         return (0);
     while (i < len)
     {
-        
+        dest[i] = origin[i];
+        i++;
     }
+    return ((void *)dest);
 }
 int main(void)
 {
     size_t  len;
-    char    str1[20] = "";
-    char    str2[20] = "";
+    char    o[20] = "Portu";
+    char    d[20] = "Cosme";
+    void    *dest;
 
     len = 3;
-    printf("ft_memmove fuction: %s\n", ft_memmove(str2, str1, len));
-    printf("memmove fuction: %s\n", memmove(str2, str1, len));
+    dest = ft_memmove(d, o, len);
+    printf("ft_memmove fuction: %s\n", (char *)dest);
+    dest = memmove(d, o, len);
+    printf("memmove fuction: %s\n", (char * )dest);
     return(0);
 }
