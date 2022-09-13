d847b2fcd1fac3d0c13238eb1e5022ec357a952a
cosisi <cosisi@MacBook-Pro-de-cosisi.local>
2022-09-13 08:15:51 +0200

    Función empezada

---
+++ ft_atoi.c
@@ -0,0 +1,17 @@
+int atoi(const char *str)
+{
+    int             dest;
+    int             sign;
+    unsigned int    i;
+
+    dest = 0;
+    sign = 1;
+    i = 0;
+    while (str[i] )
+}
+int main(void)
+{
+    const char  *str;
+
+
+}
