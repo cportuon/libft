37ff4a47428e9f41eaf7a3df784ae708f479be75
Cosme Portuondo Barcena <cportuon@c3r4s5.42urduliz.com>
2022-09-13 19:36:41 +0200

    Paco KO

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,11 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cportuon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 16:31:07 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/13 16:31:09 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/13 19:30:52 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	if (needle[j] == '\0')
+		return ((char *)haystack);
+	while (j < len)
+	{
+		if (haystack[j] == needle[0])
+		{
+			i = 0;
+			while (haystack[j + i] == needle[i]
+				&& needle[i] != '\0' && (j + 1) < len)
+			{
+				if (needle[i + 1] == '\0')
+					return ((char *)&haystack[j]);
+				i++;
+			}
+		}
+		j++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	const char	htk[] = "En Urduliz esta lloviendo";
+	const char	ndl[] = "esta";
+	size_t		n;
+
+	n = 5;
+	printf("strnstr fuction: %s\n", strnstr(htk, ndl, n));
+	printf("ft_strnstr fuction: %s\n", ft_strnstr(htk, ndl, n));
+}
