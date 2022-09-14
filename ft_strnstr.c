e54f7223f4dafb9357fa1b9379b25133f56a6f2b
Cosme Portuondo Barcena <cportuon@c2r1s5.42urduliz.com>
2022-09-14 12:29:15 +0200

    Paco OK

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 16:31:07 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/13 19:30:52 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/14 12:13:01 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,34 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	j = 0;
-	if (needle[j] == '\0')
+	i = 0;
+	if (needle[i] == '\0')
 		return ((char *)haystack);
-	while (j < len)
+	while (haystack[i] && i < len)
 	{
-		if (haystack[j] == needle[0])
+		if (haystack[i] == needle[0])
 		{
-			i = 0;
-			while (haystack[j + i] == needle[i]
-				&& needle[i] != '\0' && (j + 1) < len)
+			j = 0;
+			while (haystack[i + j] == needle[j]
+				&& needle[j] && (i + j) < len)
 			{
-				if (needle[i + 1] == '\0')
-					return ((char *)&haystack[j]);
-				i++;
+				if (needle[j + 1] == '\0')
+					return ((char *)&haystack[i]);
+				j++;
 			}
 		}
-		j++;
+		i++;
 	}
 	return (0);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	const char	htk[] = "En Urduliz esta lloviendo";
 	const char	ndl[] = "esta";
 	size_t		n;
 
-	n = 5;
+	n = 14;
 	printf("strnstr fuction: %s\n", strnstr(htk, ndl, n));
 	printf("ft_strnstr fuction: %s\n", ft_strnstr(htk, ndl, n));
-}
+}*/
