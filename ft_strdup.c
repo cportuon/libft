c31665d973dbfb5b9da26ecd17d56b8cb7af4307
Cosme Portuondo Barcena <cportuon@c3r3s6.42urduliz.com>
2022-09-21 11:31:23 +0200

    Paco OK

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,9 +6,14 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 10:15:22 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/21 10:16:08 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/21 11:26:18 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Asigna memoria suficiente para una copia de la cadena s1, hace la copia y 
+le devuelve un puntero. El puntero se puede utilizar posteriormente como 
+argumento de la función free(3). Si no hay suficiente memoria disponible, 
+se devuelve NULL y errno se establece en ENOMEM*/
 
 #include "libft.h"
 
@@ -19,7 +24,7 @@ char	*ft_strdup(const char *s1)
 	char	*s2;
 
 	len = ft_strlen(s1);
-	s2 = malloc(sizeof(char) * (len + 1));
+	s2 = malloc(sizeof(char) * len + 1);
 	if (!s2)
 		return (NULL);
 	i = 0;
@@ -28,12 +33,13 @@ char	*ft_strdup(const char *s1)
 		s2[i] = s1[i];
 		i++;
 	}
+	s2[i] = '\0';
 	return (s2);
 }
 
-/*int main(void)
+/*int	main(void)
 {
-    printf("Fuction FT_STRDUP: %s\n", ft_strdup("Good morning"));
-    printf("Fuction STRDUP: %s\n", strdup("Good morning"));
-    return (0);
+	printf("Fuction FT_STRDUP: %s\n", ft_strdup("Good morning"));
+	printf("Fuction STRDUP: %s\n", strdup("Good morning"));
+	return (0);
 }*/
