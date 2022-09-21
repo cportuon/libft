efbb62a1d8bbaea83f53395dcdeaf01b2949ee94
Cosme Portuondo Barcena <cportuon@c2r1s5.42urduliz.com>
2022-09-21 13:16:18 +0200

    Paco OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,9 +6,11 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 10:09:44 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/21 10:12:53 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/21 13:02:59 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Coge n(len)caracteres de un string (s)desde la posición inicial (start).*/
 
 #include "libft.h"
 
@@ -17,17 +19,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*dest;
 
+	if (!s)
+		return (NULL);
 	if (len > ft_strlen(s))
 		len = ft_strlen(s);
 	i = 0;
-	dest = malloc(sizeof(char) * (len + 1));
-	if (!dest || start > ft_strlen(s))
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
 		return (NULL);
-	while (i < len)
+	if (start > ft_strlen(s))
+	{
+		dest[i] = '\0';
+		return (dest);
+	}
+	while (len > 0)
 	{
 		dest[i] = s[start];
 		i++;
 		start++;
+		len--;
 	}
 	dest[i] = '\0';
 	return (dest);
@@ -40,9 +50,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t			size;
 
 	str = "cosme portuondo";
-	index = 7;
+	index = 20;
 	size = 2;
- 	printf("Fuction FT_SUBSTR: %s\n", ft_substr(str, index, size));
-	//printf("Fuction SUBSTR: %d\n", substr(str, index, size));
+	printf("Fuction FT_SUBSTR: %s\n", ft_substr(str, index, size));
 	return (0);
 }*/
