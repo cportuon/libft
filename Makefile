7d3924ef5052377eb184596ab55af831a7e1e91f
Cosme Portuondo Barcena <cportuon@c3r3s2.42urduliz.com>
2022-09-23 19:24:48 +0200

    Viernes 23

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 18:11:35 by cportuon          #+#    #+#              #
-#    Updated: 2022/09/14 18:40:54 by cportuon         ###   ########.fr        #
+#    Updated: 2022/09/23 19:09:29 by cportuon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,8 @@ SRC    = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 	ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c\
 	ft_strchr.c ft_strnstr.c ft_strrchr.c ft_strncmp.c ft_memcmp.c\
 	ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c\
-	ft_putchar_fd.c ft_putendl_fd.c ft_putstr_fd.c ft_itoa.c
+	ft_putchar_fd.c ft_putendl_fd.c ft_putstr_fd.c ft_putnbr_fd.c\
+	ft_itoa.c ft_strtrim.c
 
 # ¿Se puede sustituir por¿ --> %o : %c
 #									gcc -c -Wall -Wextra -Werror $^-o $@
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cportuon <cportuon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 12:47:29 by cportuon          #+#    #+#             */
-/*   Updated: 2022/09/21 11:38:07 by cportuon         ###   ########.fr       */
+/*   Updated: 2022/09/23 19:09:09 by cportuon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-char    *ft_itoa(int n);
+char	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -36,9 +36,11 @@ void	*ft_memset(void *str, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 char	*ft_strchr(const char *s, int coin);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char	*str);
